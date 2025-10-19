edf663693cf0b90854f64b14781d566ae7f800e1
SimoneVerzeletti <simone.verzeletti@aifos.it>
2025-10-19 18:50:55 +0200

    feat: snack 1&2

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// //SNACK 1 bici più leggera
+// const bici = [
+//   { nome: "MBK", peso: 15 },
+//   { nome: "BMX", peso: 25 },
+//   { nome: "BStreet", peso: 15 },
+//   { nome: "Speedster", peso: 10 },
+//   { nome: "RoadMax", peso: 9 },
+//   { nome: "CarbonLite", peso: 7 },
+//   { nome: "MountainKing", peso: 22 },
+//   { nome: "GravelPro", peso: 11 },
+//   { nome: "AeroRacer", peso: 8 },
+//   { nome: "EcoRide", peso: 18 },
+// ];
+
+// let biciLeggera = bici[0];
+
+// for (let i = 0; i < bici.length; i++) {
+//   const curBici = bici[i];
+//   if (curBici.peso < biciLeggera.peso) {
+//     biciLeggera = curBici;
+//   }
+// }
+// console.log(
+//   `La bici più leggera è ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg`
+// );
+
+// //SNACK 2
+// const squaCalcio = [
+//   { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+//   { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+//   { nome: "Brescia", puntiFatti: 0, falliSubiti: 0 },
+//   { nome: "Atalanta", puntiFatti: 0, falliSubiti: 0 },
+//   { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+//   { nome: "Torino", puntiFatti: 0, falliSubiti: 0 },
+// ];
+
+// for (let i = 0; i < squaCalcio.length; i++) {
+//   squaCalcio[i].puntiFatti = numeroRandom(1, 100);
+//   squaCalcio[i].falliSubiti = numeroRandom(1, 50);
+// }
+
+// let falliSubitiNomeSquadra = [];
+
+// for (let i = 0; i < squaCalcio.length; i++) {
+//   const squadra = squaCalcio[i];
+
+//   falliSubitiNomeSquadra.push({
+//     nome: squadra.nome,
+//     falliSubiti: squadra.falliSubiti,
+//   });
+// }
+
+// console.log(falliSubitiNomeSquadra);
---
+++ traccia.md
@@ -0,0 +1,27 @@
+Snack Array e Oggetti
+cartella/repo js-snack-es6
+
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo oppure in console la bici con peso minore.
+
+Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+Snack 3 (Bonus)
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Non usare i metodi di array di JS
+
+Snack 4 (Bonus)
+
+Scrivi una funzione che fonda due array
+prendendo alternativamente gli elementi da uno e dall’altro
+es. [a,b,c], [1,2,3,4,5] → [a,1,b,2,c,3,4,5].
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = ["a", "b", "c"];
